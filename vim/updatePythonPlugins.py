8a7f52f687536a67f3195d2a8dc0c6455d7c8606
Abhay Bothra <bothra90@gmail.com>
2012-07-18 02:41:20 +0530

    Update master

--- vim/updatePythonPlugins.py
+++ vim/updatePythonPlugins.py
@@ -1,7 +1,7 @@
 from os import listdir, chdir, getcwd
 from subprocess import call
 
-bundles = '/Users/abothra/.vim/bundle/'
+bundles = '~/.vim/bundle/'
 chdir(bundles)
 for bundle in listdir(bundles):
     print "updating bundle ", bundle
